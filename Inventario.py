48607d65010a5887017aa0e5d4b249839f18a45a
KevinFarias2025 <ke.fariasm@duocuc.cl>
2025-06-10 09:23:45 -0400

    Add files via upload

---
+++ Inventario.py
@@ -0,0 +1,51 @@
+
+inventario={}
+def mostrar_menu():
+    print("\nMenu")
+    print("1.Ingresa producto")
+    print("2.Buscar producto")
+    print("3.Eliminar producto")
+    print("4.Salir")
+
+while True:
+        mostrar_menu()
+        opcion=input("Elija una opcion: ")
+
+        if opcion=="1":
+            nombre=input("¿Como se llama el producto?: ")
+
+            if nombre in inventario:
+                print("Ese producto ya esta registrado") 
+            else: 
+                cantidad=input("Cantidad") 
+                precio=input("Precio") 
+
+                if cantidad.isdigit() and float(precio)> 0: 
+                    inventario[nombre]= { "cantidad":
+                  int(cantidad),"precio": float(precio)
+                    }
+                    print("Producto agregado")
+                else:
+                    print("Producto invalido")
+
+        if opcion=="2":
+             nombre=input("¿Como se llama el producto que quiere buscar?: ")
+             if nombre in inventario:
+                producto=inventario[nombre]
+                print("cantidad:"),
+                producto=["Precio"]
+             else:
+                 print("Producto no encontrado")
+        
+        elif opcion=="3":
+            nombre=input("¿Cual es el nombre del producto que desea borrar?: ")
+            if nombre in inventario:
+                del inventario[nombre]
+                print("Producto eliminado")
+            else:
+                print("El producto no existe")
+
+        elif opcion=="4":
+            print("Saliendo del menu")
+else:
+ print("Opcion invalida")
